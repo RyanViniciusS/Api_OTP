24f1d452da8e87840b90939c98b41cecb8ee5b4c
Ryan <>
2026-01-10 14:08:40 -0300

    Salvar alterações da feature/users antes de merge

--- src/db/redis.ts
+++ src/db/redis.ts
@@ -1,15 +1,15 @@
-import Redis from 'ioredis';
+// import Redis from 'ioredis';
 
-export const redisClient = new Redis({
-  host: process.env.REDIS_HOST || 'localhost',
-  port: Number(process.env.REDIS_PORT) || 6379,
-  password: process.env.REDIS_PASSWORD,
-});
+// export const redisClient = new Redis({
+//   host: process.env.REDIS_HOST || 'localhost',
+//   port: Number(process.env.REDIS_PORT) || 6379,
+//   password: process.env.REDIS_PASSWORD,
+// });
 
-redisClient.on('connect', () => {
-  console.log('✅ Conectado ao Redis');
-});
+// redisClient.on('connect', () => {
+//   console.log('✅ Conectado ao Redis');
+// });
 
-redisClient.on('error', (err) => {
-  console.error('❌ Erro ao conectar no Redis:', err);
-});
+// redisClient.on('error', (err) => {
+//   console.error('❌ Erro ao conectar no Redis:', err);
+// });
